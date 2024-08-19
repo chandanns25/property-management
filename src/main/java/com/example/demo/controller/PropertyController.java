0ba090612626d16a6c0264c89dc6fba817ac1a92
chandanns25 <chandanns2005@gmail.com>
2024-08-19 13:47:54 +0530

    AOP changes

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -5,7 +5,6 @@ import com.example.demo.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/demo/dto/UserDTO.java
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -3,6 +3,9 @@ package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +17,12 @@ public class UserDTO {
 
     private Long id;
     private String ownerName;
+    @NotNull (message = "email cannot be null")
+    @Size(min = 1,max = 50,message = "email should be between 1 to 50 characters in length")
+    @NotEmpty(message = "email cannot be blank")
     private String ownerEmail;
     private String phone;
+    @NotNull (message = "password cannot be null")
+    @NotEmpty(message = "password cannot be blank")
     private String password;
 }
